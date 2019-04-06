a5751c811fbe5fc1187cf2faef2b8527dcbd543e
Terrence <t.w.mcdonnell10@gmail.com>
2019-04-06 13:37:57 -0600

    Fix bug - input resulted in enemy/player moving

--- Terrence_scratchpad/A-Star-practice/js/Enemy.js
+++ Terrence_scratchpad/A-Star-practice/js/Enemy.js
@@ -3,26 +3,7 @@ var enemyX;
 var enemyY;
 
 function enemyMove() {
-	var nextX = enemyX;
-	var nextY = enemyY;
-
-	if(holdLeft) {
-		nextX += -ENEMY_RUN_SPEED;
-	}
-	if(holdRight) {
-		nextX += ENEMY_RUN_SPEED;
-	}
-	if(holdUp) {
-		nextY += -ENEMY_RUN_SPEED;
-	}
-	if(holdDown) {
-		nextY += ENEMY_RUN_SPEED;
-	}
-
-	if(isBrickAtPixelCoord(nextX,nextY) == false) {
-		enemyX = nextX;
-		enemyY = nextY;
-	}
+	
 }
 
 function enemyDraw() {
