8d8c4b90e340913a4b7a662d5b044f73626f0223
Terrence <t.w.mcdonnell10@gmail.com>
2019-04-08 15:55:20 -0600

    Calculate deltaTime (time between frames) and FPS + Change world layout
    Jumper.js file in this case was just reformatted to match my editor spacing file - it can be ignored

--- Terrence_scratchpad/sideview-platforming-experiment/js/Jumper.js
+++ Terrence_scratchpad/sideview-platforming-experiment/js/Jumper.js
@@ -10,49 +10,48 @@ var jumperOnGround = false;
 var jumperRadius = 10;
 
 function jumperMove() {
-   if(jumperOnGround) {
-      jumperSpeedX *= GROUND_FRICTION;
-    } else {
-      jumperSpeedX *= AIR_RESISTANCE;
-      jumperSpeedY += GRAVITY;
-      if(jumperSpeedY > jumperRadius) { // cheap test to ensure can't fall through floor
-        jumperSpeedY = jumperRadius;
-      }
-    }
+	if(jumperOnGround) {
+		jumperSpeedX *= GROUND_FRICTION;
+	} else {
+		jumperSpeedY += GRAVITY;
+		if(jumperSpeedY > jumperRadius) { // cheap test to ensure can't fall through floor
+			jumperSpeedY = jumperRadius;
+		}
+	}
   
-    if(holdLeft) {
-      jumperSpeedX = -RUN_SPEED;
-    }
-    if(holdRight) {
-      jumperSpeedX = RUN_SPEED;
-    }
+	if(holdLeft) {
+		jumperSpeedX = -RUN_SPEED;
+	}
+	if(holdRight) {
+		jumperSpeedX = RUN_SPEED;
+	}
 
-    if(jumperSpeedY < 0 && isBrickAtPixelCoord(jumperX,jumperY-jumperRadius) == 1) {
-      jumperY = (Math.floor( jumperY / BRICK_H )) * BRICK_H + jumperRadius;
-      jumperSpeedY = 0.0;
-    }
-    
-    if(jumperSpeedY > 0 && isBrickAtPixelCoord(jumperX,jumperY+jumperRadius) == 1) {
-      jumperY = (1+Math.floor( jumperY / BRICK_H )) * BRICK_H - jumperRadius;
-      jumperOnGround = true;
-      jumperSpeedY = 0;
-    } else if(isBrickAtPixelCoord(jumperX,jumperY+jumperRadius+2) == 0) {
-      jumperOnGround = false;
-    }
-    
-    if(jumperSpeedX < 0 && isBrickAtPixelCoord(jumperX-jumperRadius,jumperY) == 1) {
-      jumperX = (Math.floor( jumperX / BRICK_W )) * BRICK_W + jumperRadius;
-    }
-    if(jumperSpeedX > 0 && isBrickAtPixelCoord(jumperX+jumperRadius,jumperY) == 1) {
-      jumperX = (1+Math.floor( jumperX / BRICK_W )) * BRICK_W - jumperRadius;
-    }
-    
-    jumperX += jumperSpeedX; // move the jumper based on its current horizontal speed 
-    jumperY += jumperSpeedY; // same as above, but for vertical
-  }
-  
-  function jumperReset() {
-    // center jumper on screen
-    jumperX = canvas.width/2;
-    jumperY = canvas.height/2;
-  }
+	if(jumperSpeedY < 0 && isBrickAtPixelCoord(jumperX,jumperY-jumperRadius) == 1) {
+		jumperY = (Math.floor( jumperY / BRICK_H )) * BRICK_H + jumperRadius;
+		jumperSpeedY = 0.0;
+	}
+
+	if(jumperSpeedY > 0 && isBrickAtPixelCoord(jumperX,jumperY+jumperRadius) == 1) {
+		jumperY = (1+Math.floor( jumperY / BRICK_H )) * BRICK_H - jumperRadius;
+		jumperOnGround = true;
+		jumperSpeedY = 0;
+	} else if(isBrickAtPixelCoord(jumperX,jumperY+jumperRadius+2) == 0) {
+		jumperOnGround = false;
+	}
+
+	if(jumperSpeedX < 0 && isBrickAtPixelCoord(jumperX-jumperRadius,jumperY) == 1) {
+		jumperX = (Math.floor( jumperX / BRICK_W )) * BRICK_W + jumperRadius;
+	}
+	if(jumperSpeedX > 0 && isBrickAtPixelCoord(jumperX+jumperRadius,jumperY) == 1) {
+		jumperX = (1+Math.floor( jumperX / BRICK_W )) * BRICK_W - jumperRadius;
+	}
+
+	jumperX += jumperSpeedX; // move the jumper based on its current horizontal speed 
+	jumperY += jumperSpeedY; // same as above, but for vertical
+}
+
+function jumperReset() {
+	// center jumper on screen
+	jumperX = canvas.width/2;
+	jumperY = canvas.height/2;
+}
--- Terrence_scratchpad/sideview-platforming-experiment/js/Main.js
+++ Terrence_scratchpad/sideview-platforming-experiment/js/Main.js
@@ -1,5 +1,7 @@
 var canvas, canvasContext;
 
+const FRAMES_PER_SECOND = 30;
+
 window.onload = function() {
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
@@ -7,14 +9,47 @@ window.onload = function() {
 	initInput();
 
 	// these next few lines set up our game logic and render to happen 30 times per second
-	var framesPerSecond = 30;
 	setInterval(function() {
-	    moveEverything();
-	    drawEverything();
-	  }, 1000/framesPerSecond);
+	    update();
+	  }, 1000/FRAMES_PER_SECOND);
 	  
 	jumperReset();
 }
+
+function update() {
+	getDeltaTime();
+	moveEverything();
+	drawEverything();
+}
+
+function timeInSeconds(desiredSeconds) {
+	return seconds = desiredSeconds * FRAMES_PER_SECOND;
+}
+
+var previousTime = 0, currentTime = 0;
+var timerFull = timeInSeconds(2);
+var timer = timerFull;
+
+function getDeltaTime() {
+	frameCount++;
+	previousTime = currentTime;
+	currentTime = Date.now();
+	deltaTime = currentTime - previousTime;
+
+	//console.log("deltaTime = " + (deltaTime/1000));
+	if (timer <= 0) {
+		timeAtFPSCalc = Date.now();
+		FPS = ((frameCount/((timeAtFPSCalc - timeAtGameStart)/1000)));
+		timer = timerFull;
+	} else {
+		timer--;
+	}
+}
+
+var frameCount = 0;
+var timeAtGameStart = Date.now();
+var timeAtFPSCalc = 0;
+var FPS = 0;
 
 function drawEverything() {
 	colorRect(0, 0, canvas.width, canvas.height, 'black');
@@ -23,6 +58,8 @@ function drawEverything() {
 
 	canvasContext.fillStyle = 'white';
 	canvasContext.fillText("Arrow keys to run, spacebar to jump",8,14);
+	canvasContext.fillText("FPS = " + Math.round(FPS), canvas.width - 50, 14);
+	//canvasContext.fillText("deltaTime = " + deltaTime/1000, canvas.width - 100, 24);
 
 	colorCircle(jumperX, jumperY, jumperRadius, 'white');
 }
