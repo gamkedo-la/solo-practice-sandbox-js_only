a72acf3a9659eb68fcb6a70cd87a9075bbddc56b
Terrence <t.w.mcdonnell10@gmail.com>
2019-04-15 11:23:33 -0700

    Revert "Implement Jump script [WIP] + Other changes/refactor"
    This reverts commit 5e9fc0d2da5b7307b0e0d6e9b7ebf366d0770817.

--- Terrence_scratchpad/sideview-platforming-experiment/js/Jumper.js
+++ Terrence_scratchpad/sideview-platforming-experiment/js/Jumper.js
@@ -1,5 +1,5 @@
-const RUN_SPEED = 4;
-const JUMP_POWER = 12;
+const RUN_SPEED = 4.0;
+const JUMP_POWER = 12.0;
 const GROUND_FRICTION = 0.8;
 const AIR_RESISTANCE = 0.95;
 const GRAVITY = 0.6;
@@ -8,31 +8,17 @@ var jumperX = 75, jumperY = 75;
 var jumperSpeedX = 0, jumperSpeedY = 0;
 var jumperOnGround = false;
 var jumperRadius = 10;
-var jumping = false;
-var InitalYBeforeJump = 0;
-
-var jumpHeight = BRICK_H * 2;
-var xAtJumpHeight = Math.sqrt(jumpHeight);
-var initialVelocity = (2 * jumpHeight * (RUN_SPEED)) / xAtJumpHeight; 
-var gravityBasedOnJump = (2 * jumpHeight * Math.pow(RUN_SPEED,2)) / (xAtJumpHeight*xAtJumpHeight);
 
 function jumperMove() {
-	if (jumperOnGround) {
+	if(jumperOnGround) {
 		if (holdJump) {
-			jumping = true;
-			jumperOnGround = false;
+			jumperSpeedY = -JUMP_POWER;
 		}
 		jumperSpeedX *= GROUND_FRICTION;
 	} else {
-		if (jumping) {
-			initialVelocity = (2 * jumpHeight * (jumperSpeedX)) / xAtJumpHeight; 
-			gravityBasedOnJump = (2 * jumpHeight * Math.pow(jumperSpeedX,2)) / (xAtJumpHeight*xAtJumpHeight);
-			jumperSpeedY = jumperJump(jumperX);
-		} else {
-			jumperSpeedY += GRAVITY;
-	 		if (jumperSpeedY > jumperRadius) { // cheap test to ensure can't fall through floor
-	 			jumperSpeedY = jumperRadius;
-			}
+		jumperSpeedY += GRAVITY;
+		if(jumperSpeedY > jumperRadius) { // cheap test to ensure can't fall through floor
+			jumperSpeedY = jumperRadius;
 		}
 	}
   
@@ -45,8 +31,8 @@ function jumperMove() {
 
 	if(jumperSpeedY < 0 && isBrickAtPixelCoord(jumperX,jumperY-jumperRadius) == 1) {
 		jumperY = (Math.floor( jumperY / BRICK_H )) * BRICK_H + jumperRadius;
-		jumperSpeedY = 0;
-	} // hit head on brick
+		jumperSpeedY = 0.0;
+	}
 
 	if(jumperSpeedY > 0 && isBrickAtPixelCoord(jumperX,jumperY+jumperRadius) == 1) {
 		jumperY = (1+Math.floor( jumperY / BRICK_H )) * BRICK_H - jumperRadius;
@@ -63,21 +49,12 @@ function jumperMove() {
 		jumperX = (1+Math.floor( jumperX / BRICK_W )) * BRICK_W - jumperRadius;
 	}
 
-	jumperX += jumperSpeedX;
-	jumperY += jumperSpeedY; 
-}
-
-function jumperJump(x) {
-	var g = gravityBasedOnJump;
-	var vZero = initialVelocity;
-	if (jumperY <= InitalYBeforeJump) {
-		jumping = false;
-	}
-	return (-(1/2 * g) * (x * x) + (vZero * x) + jumperY) * deltaTime;
+	jumperX += jumperSpeedX; // move the jumper based on its current horizontal speed 
+	jumperY += jumperSpeedY; // same as above, but for vertical
 }
 
 function jumperReset() {
 	// center jumper on screen
 	jumperX = canvas.width/2 + BRICK_W/2;
-	jumperY = canvas.height/2 + BRICK_H/2;
+	jumperY = canvas.height/2;
 }
