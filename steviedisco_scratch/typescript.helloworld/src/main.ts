8aa40d8f17c25ccb633fd640a8c7edb3c52df01b
Steve Driscoll <steviedisco@gmail.com>
2019-05-04 08:29:01 +0100

    dependency injection / typescript / webpack - working

--- steviedisco_scratch/typescript.helloworld/src/main.ts
+++ steviedisco_scratch/typescript.helloworld/src/main.ts
@@ -1,7 +1,16 @@
+import { JsInject } from "lib/jsInject";
+
 export class Program 
 {
+    fn(): void
+    {
+    }
+
     execute(): void
     {
+        let $jsInject = new JsInject();                
+         $jsInject.register("1", ["echoFn", this.fn]);
+
         console.log('Hello World!');
     }
 }
--- steviedisco_scratch/typescript.helloworld/webpack.config.js
+++ steviedisco_scratch/typescript.helloworld/webpack.config.js
@@ -7,7 +7,6 @@ const path = require('path');
 /**@type {import('webpack').Configuration}*/
 const config = {
   target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-
   entry: './src/main.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
     // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
@@ -22,19 +21,19 @@ const config = {
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js', '.json'],
+    alias: {
+      lib: path.resolve(__dirname, 'lib')
+    }
   },
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader'
-          }
-        ]
-      }
+        test: /\.ts|\.tsx$/,
+        use: 'ts-loader',
+        include: [__dirname, path.resolve(__dirname, 'lib')],
+        exclude: [path.resolve(__dirname, 'node_modules')]
+      }      
     ]
   }
 };
