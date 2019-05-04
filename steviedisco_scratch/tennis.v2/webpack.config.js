5d8306eec2a686dea83d598806297b854684861c
Steve Driscoll <steviedisco@gmail.com>
2019-05-04 08:44:09 +0100

    tennis v2 initial commit

---
+++ steviedisco_scratch/tennis.v2/webpack.config.js
@@ -0,0 +1,40 @@
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+  entry: './src/main.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  output: {
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js',
+    library: "HelloWorld",
+    devtoolModuleFilenameTemplate: '../[resource-path]'
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+  },
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    extensions: ['.ts', '.js', '.json'],
+    alias: {
+      lib: path.resolve(__dirname, 'lib')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts|\.tsx$/,
+        use: 'ts-loader',
+        include: [__dirname, path.resolve(__dirname, 'lib')],
+        exclude: [path.resolve(__dirname, 'node_modules')]
+      }      
+    ]
+  }
+};
+module.exports = config;
