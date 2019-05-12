ff72df076ba061baa65ab0bb970655a46f327071
Ryan Malm <ryan.malm@gmail.com>
2019-05-12 17:15:18 -0500

    style tweaks and title change, fixed meta tags on 2nd post

--- rybar/templates/template.js
+++ rybar/templates/template.js
@@ -1,4 +1,5 @@
 function init() {
+  
   c = document.getElementById('canvas');
   ctx = c.getContext('2d');
   t = 0;
@@ -7,6 +8,11 @@ function init() {
   circleY = 0;
   c.width = 512;
   c.height = 512;
+  last = 0;
+  
+  panel.addRange("radius", 20, 200, 140, 1);
+
+  loop();
 }
 
 function loop(dt){
@@ -22,11 +28,27 @@ function loop(dt){
 }
 
 function step(dt){
-
+let rad = panel.getValue("radius");
+circleX = c.width/2 + Math.sin(t/10) * rad;
+circleY = c.height/2 + Math.cos(t/10) * rad;
 }
 
 function draw(dt){
+  //let rad = panel.getValue("radius");
+  ctx.fillStyle = '#101';
+  ctx.fillRect(0,0,c.width,c.height);let rad = panel.getValue("radius");
 
+  //draw the orbit path in a light gray
+  ctx.beginPath();
+  ctx.strokeStyle = "#999"
+  ctx.arc(c.width/2, c.height/2, rad, 0, Math.PI * 2);
+  ctx.stroke();
+
+  //draw an orbiting light purple dot
+  ctx.fillStyle = '#a88';
+  ctx.beginPath();
+  ctx.arc(circleX, circleY, 20, 0, Math.PI * 2, true);
+  ctx.fill();
 }
 
 init();
