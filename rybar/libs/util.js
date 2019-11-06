6cfba9bd6561ad49342c853666b3ed583180ff81
rybar <ryan.malm@gmail.com>
2019-11-06 00:00:22 -0600

    more lights play

--- rybar/libs/util.js
+++ rybar/libs/util.js
@@ -58,7 +58,7 @@
     }
 
     function overlapPointRect(point, rect){
-        console.log(point, rect, (point.x > rect.x1 && point.x < rect.x2 && point.y > rect.y1 && point.y < rect.y2))
+        //console.log(point, rect, (point.x > rect.x1 && point.x < rect.x2 && point.y > rect.y1 && point.y < rect.y2))
         return point.x > rect.x1 && point.x < rect.x2 && point.y > rect.y1 && point.y < rect.y2;
     }
 
