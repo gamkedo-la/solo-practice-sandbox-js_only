c7f34913e8a1552ce387cf6c0485fdd06e2e2c0b
Brian J. Boucher <brianb823@gmail.com>
2020-02-15 17:26:54 -0600

    Level and Player initial setup

--- BrianJB/js/Main.js
+++ BrianJB/js/Main.js
@@ -1,12 +1,21 @@
 var canvas;
 var canvasContext;
 
+var player;
+var grid;
+
 window.onload = function () {
 
-    console.log("onload");
+    //console.log("onload");
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
+    canvasContext.canvas.width = MAP_NUM_COLS * TILE_SIZE;
+    canvasContext.canvas.height = MAP_NUM_ROWS * TILE_SIZE;
 
+    grid = new Map();
+    player = new Player();
+
+    initRenderLoop();
 }
 
 function initRenderLoop() {
@@ -17,12 +26,16 @@ function initRenderLoop() {
         drawEverything();
 
     }, 1000 / framesPerSecond);
+    initInput();
 }
 
 function moveEverything() {
-
+    player.update();
 }
 
 function drawEverything() {
-    console.log("drawing");
+
+    grid.draw();
+    player.draw();
+
 }
