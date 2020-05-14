49c3c9f0691ec3eee57ab7540c70f3b72e37534d
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2020-05-13 22:03:29 -0600

    js testing

--- allan_regush_sandbox/hello.js
+++ allan_regush_sandbox/hello.js
@@ -1,1 +1,17 @@
 canvas = document.getElementById('canvas');
+
+
+function Ball() {
+    this.posX = 0;
+    this.posY = 0;
+}
+
+let ball = new Ball();
+
+for(let i = 0; 0 < 10; ++i) {
+    ball.posX = i;
+    ball.posY = i;
+}
+
+console.log(ball);
+
