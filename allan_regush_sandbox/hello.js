497daae9817bfb69123e34749cb994ce63c60780
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2020-08-06 17:31:25 -0600

    testing ideas

--- allan_regush_sandbox/hello.js
+++ allan_regush_sandbox/hello.js
@@ -1,17 +1,73 @@
 canvas = document.getElementById('canvas');
 
+class GameState {
+    constructor(states, initialState) {
+        this.state = states;
+        this.currentState = states[initialState]
+        this.isActive = true;
+    }
 
-function Ball() {
-    this.posX = 0;
-    this.posY = 0;
+    start() {
+        if (this.currentState != null && this.currentState != undefined) {
+            this.isActive = true;
+        }
+        this.currentState.enter();
+    }
+
+    changeState(newState) {
+        if(this.currentState == newState) return;
+        this.changeState.exit();
+        this.currentState = this.states[newState];
+        this.currentState.enter(this.currentState);
+    }
+
+    update(deltaTime) {
+        if(!this.isActive) return;
+        this.currentState.update(deltaTime);
+        const nextState = this.currentState;
+        if (nextState) this.changeState(nextState);
+    }
+
+    render() {
+        if(!this.isActive) return;
+        this.currentState.render();
+    }
+
+    getState() {
+        return this.state;
+    }
+
+    setState(state) {
+        this.state = state;
+    }
 }
 
-let ball = new Ball();
-
-for(let i = 0; 0 < 10; ++i) {
-    ball.posX = i;
-    ball.posY = i;
+class State {
+    constructor() { if (this.constructor === State) throw new Error("Can't instantiate abstract class."); }
+    enter() { if (this.constructor === State) throw new Error("Abstract Method"); }
+    update(deltaTime) { if (this.constructor === State) throw new Error("Abstract Method"); }
+    render() { if (this.constructor === State) throw new Error("Abstract Method"); }
+    exit() { if (this.constructor === State) throw new Error("Abstract Method"); }
 }
 
-console.log(ball);
+class  Menu extends State {
+    constructor() {
 
+    }
+
+    enter() {
+
+    }
+
+    update(deltaTime) {
+
+    }
+
+    render() {
+
+    }
+
+    exit() {
+        
+    }
+}
