26fdb8c79a3c9690f5a42731a24b04f9fb697293
Charlene A <chichiko90@gmail.com>
2020-08-30 20:35:35 +0800

    Added menu screen for shattered clone

--- Charlene_ScribblesCoding/shatteredpixeldungeon_clone/js/Main.js
+++ Charlene_ScribblesCoding/shatteredpixeldungeon_clone/js/Main.js
@@ -7,12 +7,26 @@ window.onload = function() {
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
   
-  loadImages();
+  menu();
+
+  canvas.addEventListener('click', function(e) {
+    loadImages();
+  });
+}
+
+function menu() {
+  canvasContext.font = "48px serif";
+  canvasContext.fillStyle = "black";
+  canvasContext.fillText("Welcome to Shattered Pixel Dungeon clone!", 20, 200);
+  canvasContext.fillText("Dungeon clone!", 200, 250);
+  canvasContext.font = "30px serif";
+  canvasContext.fillText("Click anywhere to start playing", 180, 300);
 }
 
 function loadingDoneSoStartGame() {
   // these next few lines set up our game logic and render to happen 30 times per second
   var framesPerSecond = 30;
+  
   setInterval(function() {
       moveEverything();
       drawEverything();
