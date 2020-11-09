afdce05a0f7b6ee6c5bfc21885f49336a458b6ca
stebssbets <stebssbets@yahoo.com>
2020-11-09 18:28:56 +0700

    adding comments to camera.js to help me think through debugging

--- Stebs/Snatch/camera.js
+++ Stebs/Snatch/camera.js
@@ -9,45 +9,37 @@ function Camera () {
 		if(controlCameraForDebug) {return;}
 		this.deltaCanvasCenterXWithTarget = target.centerX - (canvas.width/2); //   /scaleWidth;
 		this.deltaCanvasCenterYWithTarget = target.centerY - (canvas.height/2); //   /scaleHeight;
-		// this.verticalCenterOfCanvas = canvas.width/2;
-		// this.horizontalCenterOfCanvas = canvas.height/2;
 
-		// this.checkIfScooterIsAtVerticalCenterOfCanvas = function()
-		// {
-		// 	if ( Math.abs(scooter.centerX - canvas.width/2) < 10)
-		// 	{
-		// 		return true;
-		// 	}
-		// }
-
-		// this.checkIfScooterIsAtHorizontalCenterOfCanvas = function()
-		// {
-		// 	if ( Math.abs(scooter.centerY - canvas.height/2) < 10)
-		// 	{
-		// 		return true;
-		// 	}
-		// }
-
-		// if(this.deltaCanvasCenterXWithTarget < 0){
-		// 	console.log('this.deltaCanvasCenterXWithTarget: ' + this.deltaCanvasCenterXWithTarget);
-		// 	this.deltaCanvasCenterXWithTarget = 0;
-		// }
-		// if(this.deltaCanvasCenterYWithTarget < 0){
-		// 	console.log('this.deltaCanvasCenterYWithTarget: ' + this.deltaCanvasCenterYWithTarget);
-		// 	this.deltaCanvasCenterYWithTarget = 0;
-		// }
-		
-		var rightEdgeOfCanvasX = TRACK_WIDTH * NUMBER_OF_COLUMNS;
-		var leftEdgeOfCanvasX = 0;
-		var bottomEdgeOfCanvasY = TRACK_HEIGHT * NUMBER_OF_ROWS;
-		var leftEdgeOfCanvasY = 0;
-
-		
-		if(this.deltaCanvasCenterXWithTarget >= rightEdgeOfCanvasX - 1 - canvas.width  /*/scaleWidth*/  ){
-			this.deltaCanvasCenterXWithTarget = (rightEdgeOfCanvasX - 1 - canvas.width)  /*/scaleWidth*/ ;
+		//if on the right half of canvas don't pan???
+		if(this.deltaCanvasCenterXWithTarget < 0)
+		{
+			console.log('this.deltaCanvasCenterXWithTarget: ' + this.deltaCanvasCenterXWithTarget);
+			this.deltaCanvasCenterXWithTarget = 0;
 		}
-		if(this.deltaCanvasCenterYWithTarget >= bottomEdgeOfCanvasY - 1 - canvas.height  /*/scaleHeight*/  ){
-			this.deltaCanvasCenterYWithTarget = bottomEdgeOfCanvasY - 1 - canvas.height  /*/scaleHeight*/  ;
+		//if on the top half of canvas don't pan????
+		if(this.deltaCanvasCenterYWithTarget < 0)
+		{
+			console.log('this.deltaCanvasCenterYWithTarget: ' + this.deltaCanvasCenterYWithTarget);
+			this.deltaCanvasCenterYWithTarget = 0;
+		}
+		
+		var rightEdgeOfMapX = TRACK_WIDTH * NUMBER_OF_COLUMNS;
+		var leftEdgeOfMapX = 0;
+		var bottomEdgeOfMapY = TRACK_HEIGHT * NUMBER_OF_ROWS;
+		var topEdgeOfMapY = 0;
+
+		var lastFullCanvasWidthOnRightSideMinus1 = rightEdgeOfMapX - 1 - canvas.width;
+		var lastFullCanvasHeightOnBottomSideMinus1 = bottomEdgeOfMapY - 1 - canvas.height;
+
+		//don't pan past right edge of screen???
+		if(this.deltaCanvasCenterXWithTarget >= lastFullCanvasWidthOnRightSideMinus1/*/scaleWidth*/  )
+		{
+			this.deltaCanvasCenterXWithTarget = lastFullCanvasWidthOnRightSideMinus1  /*/scaleWidth*/ ;
+		}
+		//don't pan past bottom edge of screen???
+		if(this.deltaCanvasCenterYWithTarget >= lastFullCanvasHeightOnBottomSideMinus1  /*/scaleHeight*/  )
+		{
+			this.deltaCanvasCenterYWithTarget = lastFullCanvasHeightOnBottomSideMinus1  /*/scaleHeight*/  ;
 		}
 	};
 
@@ -65,25 +57,7 @@ function Camera () {
 			shakeX = Math.floor(Math.random() * 3 - 1) * this.shakePower;
 			shakeY = Math.floor(Math.random() * 3 - 1) * this.shakePower;
 		}
-		
-		if (this.deltaCanvasCenterXWithTarget > 0)
-		{
-			ctx.translate(-this.deltaCanvasCenterXWithTarget + shakeX, 0);
-		}
-		else if (this.deltaCanvasCenterXWithTarget < 0)
-		{
-			ctx.translate(this.deltaCanvasCenterXWithTarget + shakeX, 0);
-		}
-
-		if (this.deltaCanvasCenterYWithTarget > 0)
-		{
-			ctx.translate(0, -this.deltaCanvasCenterYWithTarget + shakeY);
-		}
-		else if (this.deltaCanvasCenterYWithTarget > 0)
-		{
-			ctx.translate(0, this.deltaCanvasCenterYWithTarget + shakeY);
-		}
-		
+		ctx.translate(-this.deltaCanvasCenterXWithTarget + shakeX, -this.deltaCanvasCenterYWithTarget + shakeY);
 	};
 
 	this.endPan = function (ctx = canvasContext) {
