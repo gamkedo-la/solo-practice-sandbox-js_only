b42c027243d5117d61d3aaeebc0d7637bcffb323
Gamkedo <chris@gamkedo.com>
2020-11-11 10:44:09 -0800

    Migrating Stebs prototype getting ready for pitch from sandbox into separate repo

--- Stebs/Snatch/audio.js
+++
@@ -1,5 +0,0 @@
-var orderAlertSFXAudioTag = document.createElement("audio");
-orderAlertSFXAudioTag.src = 'audio/orderAlert.mp3';
-
-var pickupOrDropoffSFXAudioTag = document.createElement("audio");
-pickupOrDropoffSFXAudioTag.src = 'audio/pickupOrDropoff.mp3';
