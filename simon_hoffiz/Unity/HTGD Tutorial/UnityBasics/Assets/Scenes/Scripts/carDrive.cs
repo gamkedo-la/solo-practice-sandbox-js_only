dd7675277945cc40f169238c007f903ad61a1d80
Simon <sj.hoffiz@gmail.com>
2020-12-17 10:52:39 -0500

    started unity tutorial series

---
+++ simon_hoffiz/Unity/HTGD Tutorial/UnityBasics/Assets/Scenes/Scripts/carDrive.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carDrive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("car object named" + gameObject.name + "started script");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(Input.GetKey(KeyCode.UpArrow)) {
+            transform.position += transform.forward * Time.deltaTime * 3.0f;
+        }
+        if(Input.GetKey(KeyCode.DownArrow)) {
+            transform.position += transform.forward * Time.deltaTime * -3.0f;
+        }*/
+            transform.position += transform.forward * Time.deltaTime * 3.0f *
+            Input.GetAxis("Vertical");
+
+        /*if(Input.GetKey(KeyCode.RightArrow)) {
+            transform.Rotate(Vector3.up, 30.0f * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Rotate(Vector3.up, -30.0f * Time.deltaTime);
+        } */
+        transform.Rotate(Vector3.up, 30.0f * Time.deltaTime *
+            Input.GetAxis("Horizontal"));
+        
+    }
+}
