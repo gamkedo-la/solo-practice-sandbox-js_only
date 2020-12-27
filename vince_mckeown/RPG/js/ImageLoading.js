389ad65aeab9ee82fe9521b287e1229006a2b511
Vince McKeown <vmckeown79@gmail.com>
2020-12-27 10:32:26 -0500

    Initial Code for RPG

---
+++ vince_mckeown/RPG/js/ImageLoading.js
@@ -0,0 +1,57 @@
+var playerPic=document.createElement("img");
+var tilePics = [];
+
+var picsToLoad = 0;
+
+function countLoadedImageAndLaunchIfReady() {
+  picsToLoad--;
+  if(picsToLoad == 0) { // last image loaded?
+    loadingDoneSoStartGame();
+  }
+}
+
+function beginLoadingImage(imgVar, fileName) {
+  imgVar.onload=countLoadedImageAndLaunchIfReady;
+  imgVar.src="images/"+fileName;
+}
+
+function loadImageForTileCode(tileCode, fileName) {
+  tilePics[tileCode] = document.createElement("img");
+  beginLoadingImage(tilePics[tileCode],fileName);
+}
+
+function loadImages() {
+
+  var imageList = [
+    {varName:playerPic, theFile:"warrior.png"},
+    
+    {tileType:TILE_GROUND, theFile:"world_ground.png"},
+    {tileType:TILE_WALL_1, theFile:"dungeonWall_1.png"},
+    {tileType:TILE_WALL_2, theFile:"dungeonWall_2.png"},
+    {tileType:TILE_WALL_3, theFile:"dungeonWall_3.png"},
+    {tileType:TILE_WALL_4, theFile:"dungeonWall_4.png"},
+    {tileType:TILE_WALL_5, theFile:"dungeonWall_5.png"},
+    {tileType:TILE_WALL_6, theFile:"dungeonWall_6.png"},
+	{tileType:TILE_WALL_7, theFile:"dungeonWall_7.png"},
+    {tileType:TILE_WALL_8, theFile:"dungeonWall_8.png"},
+    {tileType:TILE_WALL_9, theFile:"dungeonWall_9.png"},
+	{tileType:TILE_WALL_10, theFile:"dungeonWall_10.png"},
+	{tileType:TILE_WALL_11, theFile:"dungeonWall_11.png"},
+	{tileType:TILE_WALL_12, theFile:"dungeonWall_12.png"},
+	{tileType:TILE_WALL_13, theFile:"dungeonWall_13.png"},
+    {tileType:TILE_GOAL, theFile:"world_goal.png"},
+    {tileType:TILE_KEY, theFile:"world_key.png"},
+    {tileType:TILE_DOOR, theFile:"world_door.png"}
+    ];
+
+  picsToLoad = imageList.length;
+
+  for(var i=0;i<imageList.length;i++) {
+    if(imageList[i].tileType != undefined) {
+      loadImageForTileCode(imageList[i].tileType, imageList[i].theFile);
+    } else {
+      beginLoadingImage(imageList[i].varName, imageList[i].theFile);
+    } // end of else
+  } // end of for imageList
+
+} // end of function loadImages
---
+++ vince_mckeown/RPG/js/World.js
@@ -0,0 +1,96 @@
+// world, room, and tile constants, variables
+const ROOM_COLS = 16;
+const ROOM_ROWS = 12;
+
+var roomGrid =
+    [ 11, 1, 1, 1, 1, 1,16, 1, 1, 1,16, 1,16, 1, 1, 9,
+      11, 0, 0, 0, 0, 0,15, 0, 0, 0, 5, 0,15, 0, 0, 9,
+      11, 0, 4, 0, 4, 0,15, 0, 2, 0,15, 0,15, 4, 4, 9,
+      11, 0, 0, 0, 0, 0,15, 0, 0, 0,15, 5, 1, 5, 0, 9,
+      11, 1, 1, 5, 1, 1,14, 0, 4, 0,15, 0, 0, 0, 0, 9,
+      11, 0, 0, 0, 0, 0, 0, 0, 0, 0,15, 0, 4, 0, 0, 9,
+      11, 0, 0, 0, 0, 0, 0, 0, 0, 0,15, 0, 0, 0, 0, 9,
+      11, 0,16, 1,16, 1,16, 1, 1, 1,15, 0, 4, 0, 0, 9,
+      11, 0,15, 0,15, 0,15, 0, 0, 0,15, 0, 0, 0, 0, 9,
+      11, 0, 5, 0, 5, 0, 5, 0, 3, 0,15, 0, 0, 0, 0, 9,
+      11, 0,15, 0,15, 0,15, 0, 0, 0,15, 0, 0, 0, 0, 9,
+      17, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,18];
+
+const TILE_W = 50;
+const TILE_H = 50;
+
+const TILE_GROUND = 0;
+const TILE_PLAYER = 2;
+const TILE_GOAL = 3;
+const TILE_KEY = 4;
+const TILE_DOOR = 5;
+const TILE_WALL_1 = 6;
+const TILE_WALL_2 = 7;
+const TILE_WALL_3 = 8;
+const TILE_WALL_4 = 9;
+const TILE_WALL_5 = 10;
+const TILE_WALL_6 = 11;
+const TILE_WALL_7 = 12;
+const TILE_WALL_8 = 1;
+const TILE_WALL_9 = 14;
+const TILE_WALL_10 = 15;
+const TILE_WALL_11 = 16;
+const TILE_WALL_12 = 17;
+const TILE_WALL_13 = 18;
+
+
+function roomTileToIndex(tileCol, tileRow) {
+  return (tileCol + ROOM_COLS*tileRow);
+}
+
+function getTileIndexAtPixelCoord(pixelX,pixelY) {
+  var tileCol = pixelX / TILE_W;
+  var tileRow = pixelY / TILE_H;
+  
+  // we'll use Math.floor to round down to the nearest whole number
+  tileCol = Math.floor( tileCol );
+  tileRow = Math.floor( tileRow );
+
+  // first check whether the tile coords fall within valid bounds
+  if(tileCol < 0 || tileCol >= ROOM_COLS ||
+     tileRow < 0 || tileRow >= ROOM_ROWS) {
+     document.getElementById("debugText").innerHTML = "out of bounds:"+pixelX+","+pixelY;
+     return undefined;
+  }
+  
+  var tileIndex = roomTileToIndex(tileCol, tileRow);
+  return tileIndex;
+}
+
+function tileTypeHasTransparency(checkTileType) {
+  return (checkTileType == TILE_GOAL ||
+          checkTileType == TILE_KEY ||
+          checkTileType == TILE_DOOR);
+}
+
+function drawRoom() {
+  var tileIndex = 0;
+  var tileLeftEdgeX = 0;
+  var tileTopEdgeY = 0;
+  
+  for(var eachRow=0; eachRow<ROOM_ROWS; eachRow++) { // deal with one row at a time
+    
+    tileLeftEdgeX = 0; // resetting horizontal draw position for tiles to left edge
+    
+    for(var eachCol=0; eachCol<ROOM_COLS; eachCol++) { // left to right in each row
+
+      var tileTypeHere = roomGrid[ tileIndex ]; // getting the tile code for this index
+      if( tileTypeHasTransparency(tileTypeHere) ) {
+        canvasContext.drawImage(tilePics[TILE_GROUND], tileLeftEdgeX, tileTopEdgeY);
+      }
+      canvasContext.drawImage(tilePics[tileTypeHere], tileLeftEdgeX, tileTopEdgeY);
+      
+      tileIndex++; // increment which index we're going to next check for in the room
+      tileLeftEdgeX += TILE_W; // jump horizontal draw position to next tile over by tile width
+
+    } // end of for eachCol
+    
+    tileTopEdgeY += TILE_H; // jump horizontal draw position down by one full tile height
+    
+  } // end of for eachRow    
+} // end of drawRoom()
