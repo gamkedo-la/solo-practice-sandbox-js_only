8d481e9db9a468163cb37aa8bd69fa07d36681c8
Ryan Cahela <ryancahela@gmail.com>
2021-01-17 11:55:50 -0800

    red square bounces around

--- Ryan_Cahela/00_Brick_Breaker/script.js
+++ Ryan_Cahela/00_Brick_Breaker/script.js
@@ -1,5 +1,37 @@
 const canvas = document.getElementById("board");
 const ctx = canvas.getContext("2d");
+const WIDTH = canvas.width;
+const HEIGHT = canvas.height;
 
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, 100, 100);
+let deltaTime = 0;
+let timeOfLastFrame = 0;
+
+const square = {
+  position: { x: 0, y: 0 },
+  direction: { x: 1, y: 0 },
+  color: "red",
+  speed: 500,
+};
+
+function loop(totalTimeElapsedMiliseconds) {
+  requestAnimationFrame(loop);
+
+  const currentTime = totalTimeElapsedMiliseconds / 1000; //convert to seconds
+  const deltaTime = currentTime - timeOfLastFrame;
+  timeOfLastFrame = currentTime;
+
+  //clear game screen
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+  //move cube
+  if (square.position.x > WIDTH || square.position.x < 0) {
+    square.direction.x = -square.direction.x;
+  }
+  square.position.x += square.speed * square.direction.x * deltaTime;
+
+  //draw cube
+  ctx.fillStyle = square.color;
+  ctx.fillRect(square.position.x, square.position.y, 100, 100);
+}
+
+requestAnimationFrame(loop);
