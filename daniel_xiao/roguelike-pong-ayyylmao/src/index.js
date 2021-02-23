ace74d56c0d619aee5bb98b717992e656c53568a
aahvocado <dan.dan.makes.stuff@gmail.com>
2021-02-22 22:19:42 -0800

    style up the page

--- daniel_xiao/roguelike-pong-ayyylmao/src/index.js
+++ daniel_xiao/roguelike-pong-ayyylmao/src/index.js
@@ -1,11 +1,1 @@
 import 'styles/main.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = 'hello world';
-
-  return element;
-}
-
-document.body.appendChild(component());
