53dfd7a5f2ce4ce1cf6ff3429cc86374c3a18b38
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2021-03-13 22:14:17 -0600

    grid

--- allan_regush_sandbox/racing/index.js
+++ allan_regush_sandbox/racing/index.js
@@ -1,7 +1,13 @@
-var canvas = document.getElementById('gameCanvas');
-var canvasWidth  = canvas.width;
-var canvasHeight = canvas.height;
-var ctx = canvas.getContext('2d');
+const canvas = document.getElementById('gameCanvas');
+const canvasWidth  = canvas.width;
+const canvasHeight = canvas.height;
+const ctx = canvas.getContext('2d');
+
+const BRICK_WIDTH = 100;
+const BRICK_HEIGHT = 100;
+const BRICK_COLS = 8;
+const BRICK_ROWS = 6;
+const BRICK_GAP = 2;
 document.addEventListener('keydown', handleKey);
 
 const gameState = {
@@ -38,8 +44,30 @@ function main() {
 }
 
 function render() {
-  ctx.fillStyle = 'red';
-  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  blackoutCanvas();
   ctx.fillStyle = 'blue';
   ctx.fillRect(gameState.p1.x, gameState.p1.y, 10, 10);
+  drawGrid();
 }
+
+function drawGrid() {
+  for(let col = 0; col < BRICK_COLS; ++col) {
+    for(let row = 0; row < BRICK_ROWS; ++row) {
+      const brickLeftEdgeX = col * BRICK_WIDTH;
+      const brickTopEdgeY = row *  BRICK_HEIGHT;
+      drawRectangle(brickLeftEdgeX, brickTopEdgeY,
+        BRICK_WIDTH - BRICK_GAP, BRICK_HEIGHT - BRICK_GAP, 'blue');
+    }
+  }
+}
+
+function blackoutCanvas() {
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0,0, canvas.width, canvas.height);
+}
+
+function drawRectangle(x, y, width, height, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+}
+
