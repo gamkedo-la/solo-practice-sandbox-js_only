5dd3e6591453573ec76d644a64ff9edb704b90a8
Simon Donohoe <simon.donohoe@gmail.com>
2021-04-22 22:38:19 +0100

    Have the ball bouncing off all sides

--- Simon_Donohoe/starting over/brickBreaker/js/main.js
+++ Simon_Donohoe/starting over/brickBreaker/js/main.js
@@ -1,21 +1,39 @@
+// ball variables
 let ballX = 75, ballY = 75;
+let ballSpeedX = 2;
+let ballSpeedY = 2;
+
+// canvas variables
 let canvas, canvasContext;
 
 window.onload = function(){
   // game canvas
-  canvas = document.createElement('gameCanvas');
+  canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
 
-  setInterval(drawEverything, 1000);
+  let framesPerSecond = 60;
+  setInterval(function(){ 
+    moveEverything();
+    drawEverything();
+  }, 1000/framesPerSecond);
+}
+
+function moveEverything(){
+  if(ballX > canvas.width || ballX < 0){ // if the ball has moved past the left or right edge
+    ballSpeedX *= -1; // reverse the balls direction
+  }
+  if(ballY > canvas.height || ballY < 0){ // if the ball moves past the top or bottom
+    ballSpeedY *= -1; // reverse the balls direction
+  }
+  // move the ball to the right
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
 }
 
 function drawEverything(){
   // fill game canvas with black
   canvasContext.fillStyle = "black";
   canvasContext.fillRect(0,0, canvas.width, canvas.height);
-
-  ballX += 50;
-  console.log("ballX is now: " + ballX);
 
   // draw a circle(game ball)
   canvasContext.fillStyle = "white";
