2d0ee78a552bf498e7338b2f5305802480d34853
Rodrigo Lopez <rodprogramdev@gmail.com>
2021-08-09 18:01:38 +0800

    update Hero.js & World.js

--- Bonzerr-practice-js/js/Hero.js
+++ Bonzerr-practice-js/js/Hero.js
@@ -1,6 +1,6 @@
 // const GROUNDSPEED_DECAY_MULT = 0.94;
-const DRIVE_POWER = 0.5;
-const REVERSE_POWER = 0.2;
+// const DRIVE_POWER = 0.5;
+// const REVERSE_POWER = 0.2;
 // const TURN_RATE = 0.06;
 // const MIN_SPEED_TO_TURN = 0.5;
 const PLAYER_MOVEMENT_SPEED = 3.0;
