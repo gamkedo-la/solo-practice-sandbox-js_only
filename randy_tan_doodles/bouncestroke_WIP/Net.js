a6a61fde1c7f61ca02dfc47516f1420ef38ca35b
Randy Tan Shaoxian <oneseedfruit@gmail.com>
2022-02-20 20:23:39 +0800

    position offset for the net on collision with the rock

--- randy_tan_doodles/bouncestroke_WIP/Net.js
+++ randy_tan_doodles/bouncestroke_WIP/Net.js
@@ -5,10 +5,11 @@ const Net = function(cvs = canvas, ctx = canvasContext) {
     this.x = cvs.width * 0.5;
     this.y = cvs.height - this.h;
 
+    this.vibration = 0;
+
     this.colCheck = new Collision();
 
-    this.update = (dt, col) => {        
-        this.x = cvs.width * 0.5;
+    this.update = (dt, col) => {                
         this.y = cvs.height - this.h;
 
         col.forEach(c => {
@@ -25,15 +26,37 @@ const Net = function(cvs = canvas, ctx = canvasContext) {
                         this.x, this.y, this.x + this.w, this.y + this.h,
                         c.x1, c.y1, c.x2, c.y2)) {
                         console.log("Net detects collision!");
+                        
+                        if (c.x1 < this.x) {                            
+                            this.vibration += 70;
+                        }
+                        else if (c.x2 > this.x + this.w) {
+                            this.vibration -= 70;
+                        }
                     }
                 }
             }
         });
+
+        if (Math.abs(this.vibration) > 0) {
+            if (this.vibration > 0)
+                this.vibration -= 1000 * dt;
+            else if (this.vibration < 0)
+                this.vibration += 1000 * dt;
+        }
+
+        if (this.x > cvs.width * 0.5) {
+            this.x -= 100 * dt;
+        }
+        if (this.x < cvs.width * 0.5) {
+            this.x += 100 * dt;
+        }
+
+        this.x += this.vibration * dt;
     };
 
     this.render = dt => {
         Draw.rect(this.x, this.y, this.w, this.h, "black");
         Draw.outlineRect(this.x, this.y, this.w, this.h, "Magenta", 2);
-
     };
 };
