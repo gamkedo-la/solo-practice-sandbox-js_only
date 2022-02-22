c814174b478fb70c82ef8238738851848c640e87
Gonzalo Delgado <mail@gonzalodelgado.com>
2022-02-22 10:11:49 -0300

    Set up basic grid for placing targets randomly.

--- gonzalo/gallery/js/level.js
+++ gonzalo/gallery/js/level.js
@@ -1,15 +1,33 @@
 export class Level {
   static maxTargets = 10;
+  static gridWidth = 20;
+  static gridHeight = 11;
+  static tileSize = 16; // pixels
+
+  static getXForGridIndex(i) {
+	return i % Level.gridWidth;
+  }
+
+  static getYForGridIndex(i) {
+	return Math.floor(i/Level.gridWidth);
+  }
 
   constructor(ctx) {
 	this.ctx = ctx;
-	const row = Math.round(this.ctx.canvas.height/4);
-	const col = Math.round(this.ctx.canvas.width/3);
-	this.targets = [
-	  {x: col, y: row, live: true},
-	  {x: col*1.5, y: row, live: true},
-	  {x: col*2, y: row, live: true},
-	];
+
+	const gridSize = Level.gridWidth * Level.gridHeight;
+	// Populate targets in grid
+	this.targets = {};
+	while (Object.keys(this.targets).length < Level.maxTargets) {
+	  const i = Math.floor(Math.random() * gridSize);
+	  if (!Object.keys(this.targets).includes(i)) {
+		this.targets[i] = {
+		  live: true,
+		  x: Level.getXForGridIndex(i)*Level.tileSize,
+		  y: Level.getYForGridIndex(i)*Level.tileSize,
+		};
+	  }
+	}
   }
 
   update(dt) {
@@ -18,7 +36,7 @@ export class Level {
 
   draw(dt) {
 	const ctx = this.ctx;
-	this.targets.filter(target => target.live).forEach(target => {
+	Object.values(this.targets).filter(target => target.live).forEach(target => {
 	  ctx.fillStyle = "pink";
 	  ctx.beginPath();
 	  ctx.arc(target.x, target.y, 10, 0, 2*Math.PI);
@@ -30,7 +48,7 @@ export class Level {
   getHitTargetHook() {
 	const targets = this.targets;
 	const hitTargetHook = function(dt, shot) {
-	  targets.filter(target => target.live).forEach(target => {
+	  Object.values(targets).filter(target => target.live).forEach(target => {
 		const dist = Math.sqrt(Math.pow(target.x - shot.target.x, 2) + Math.pow(target.y - shot.target.y, 2));
 		if (dist <= 16) {
 		  target.live = false;
