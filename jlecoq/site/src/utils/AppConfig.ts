26bc0f13c568e30627eb5b19530960dda2dfa65b
Jacob LeCoq <lecoqjacob@gmail.com>
2023-05-04 15:34:25 -0500

    :green_heart: Remove site folder to address failing CI

--- jlecoq/site/src/utils/AppConfig.ts
+++
@@ -1,8 +0,0 @@
-export const AppConfig = {
-	site_name: "Yendor's World",
-	title: "Yendor's World",
-	description: 'Site to facilitate my learnings',
-	author: 'Yendor',
-	locale_region: 'en-us',
-	locale: 'en',
-};
--- jlecoq/site/src/utils/Posts.ts
+++
@@ -1,9 +0,0 @@
-import type { IFrontmatter, MarkdownInstance } from 'astro-boilerplate-components';
-
-export const sortByDate = (
-	posts: Array<MarkdownInstance<IFrontmatter>>,
-): Array<MarkdownInstance<IFrontmatter>> => {
-	return posts.sort(
-		(a, b) => new Date(b.frontmatter.pubDate).valueOf() - new Date(a.frontmatter.pubDate).valueOf(),
-	);
-};
