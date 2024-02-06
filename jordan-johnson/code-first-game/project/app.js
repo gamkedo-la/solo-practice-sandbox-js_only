6da1d218b3db53d1a27840c3d9e7cf724c3d4d97
Jordan <jordan33111@gmail.com>
2024-02-06 17:28:41 -0500

    movement and time
    put draw code into a functioned used set interval call function more than once

--- jordan-johnson/code-first-game/project/app.js
+++ jordan-johnson/code-first-game/project/app.js
@@ -1,8 +1,17 @@
 console.log('Hello World');
-
+var ballX = 50;
 var canvas = document.getElementById('gameCanvas');
 var canvasContext = canvas.getContext('2d');
-canvasContext.fillStyle = 'Black';
-canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-canvasContext.fillStyle = 'red';
-canvasContext.fillRect(120, 200, 50, 25);
+
+setInterval(drawEverything, 1);
+
+function drawEverything() {
+	ballX += 1;
+	canvasContext.fillStyle = 'Black';
+	canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+	canvasContext.fillStyle = 'White';
+	canvasContext.fillRect(225, 210, 200, 200);
+	canvasContext.fillStyle = 'red';
+	canvasContext.fillRect(ballX, 200, 10, 10);
+}
---
+++ jordan-johnson/code-first-game/project/readme.md
@@ -0,0 +1,15 @@
+### Intro
+
+### Drawing and Positions
+
+### Movement and Time
+
+Create a function to group all Draw Code
+
+- created a draw everything function to story the for drawing the ball
+
+- create a timer for this method so we know when to call it.
+
+- use setInterval in JS to do this
+
+- made the small on the screen round
