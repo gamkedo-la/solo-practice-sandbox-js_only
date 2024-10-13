c94fe2c693135eea48c2e519d5526023c74fec5c
Allan Regush <allan@allanregush.com>
2024-10-12 18:39:31 -0600

    AR: Mouse Move

--- allan_regush_sandbox/warrior/index.js
+++ allan_regush_sandbox/warrior/index.js
@@ -16,9 +16,22 @@ window.onload = function() {
       console.error("Could Not Get Context");
       return;
     }
+    canvas.addEventListener('mousemove', function(evt) {
+        const mousePos = calculateMousePos(evt);
+    });
     requestAnimationFrame(main);
 }
 
+function calculateMousePos(evt) {
+    const root = document.documentElement;
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = evt.clientX - rect.left - root.scrollLeft;
+    const mouseY = evt.clientY - rect.top - root.scrollTop;
+    return {
+        x: mouseX,
+        y: mouseY
+    };
+}
 
 
 function main(timeStamp) {
